6a6dc1085a740531bb02b08162c1542fe38ae498
Luisnefelibato <luisfgomezosp@gmail.com>
2025-05-22 00:05:24 -0500

    mEJORAS

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,15 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  server: {
-    port: 3000,
-    host: true,
-    cors: true
-  },
+  base: '/',
   build: {
     outDir: 'dist',
+    assetsDir: 'assets',
     sourcemap: false,
     minify: 'terser'
   },
-  define: {
-    // Definir variables globales si es necesario
-    __API_URL__: JSON.stringify('http://173.249.8.251:5000')
+  server: {
+    port: 3000,
+    host: true
   }
 })
